dc1b0d33d985c1500ef8b45f09a54ebdc14d604c
kurog1ri <kosenko.yosiki@gmail.com>
2025-05-04 14:21:51 +0300

    Initial Commit

---
+++ EchocodeTaskTest/EchocodeTaskTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchocodeTaskTestApp.swift
+//  EchocodeTaskTest
+//
+//  Created by   Kosenko Mykola on 04.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct EchocodeTaskTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
